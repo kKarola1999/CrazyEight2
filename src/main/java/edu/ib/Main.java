c4a143a79273f9bdc005b08bca12fda0ff597c4a
Karolina <250013@student.pwr.edu.pl>
2021-01-25 22:41:51 +0100

    Initial commit

---
+++ src/main/java/edu/ib/Main.java
@@ -0,0 +1,25 @@
+package edu.ib;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/DeckView.fxml"));
+        Scene scene = new Scene(root);
+
+        primaryStage.setTitle("Deck Of Cards");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
+
